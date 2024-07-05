da5f07e8474cf68fdd7efa683bd95b08bf3ef8de
Jason Sejik Lee <gggabikr@gmail.com>
2024-07-04 22:52:32 -0700

    Update api_service.dart

--- toonflix/lib/services/api_service.dart
+++ toonflix/lib/services/api_service.dart
@@ -1,16 +1,27 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
+  List<WebtoonModel> webtoonInstances = [];
   final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
       //it means the request is successful
-      print(response.body);
-      return; //여기서 함수를 끝낸다.
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+        // var title = toon.title;
+        // var id = toon.id;
+        // print('id: $id, title: $title');
+      }
+
+      return webtoonInstances; //여기서 함수를 끝낸다.
     }
     throw Error(); //만약 if 부분이 실행되지않으면 에러를 발생시킨다.
   }
