65f4a157b81266b3f6fdf004d51e1cfbff651ae8
Jason Sejik Lee <gggabikr@gmail.com>
2024-07-09 10:30:10 -0700

    Update home_screen.dart

--- toonflix/lib/screens/home_screen.dart
+++ toonflix/lib/screens/home_screen.dart
@@ -28,9 +28,19 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const Text('There is data!');
+            return ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                print(index);
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+            );
           } else {
-            return const Text('Loading...');
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           }
         },
       ),
