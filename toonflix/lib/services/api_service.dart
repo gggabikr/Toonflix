430fccff1a32a875e0f11d9661c206426d51b35a
Jason Sejik Lee <gggabikr@gmail.com>
2024-07-13 20:39:27 -0700

    Update api_service.dart

--- toonflix/lib/services/api_service.dart
+++ toonflix/lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
@@ -42,13 +43,20 @@ class ApiService {
     throw Error();
   }
 
-  static Future<WebtoonDetailModel> getToonById(String id) async {
-    final url = Uri.parse("$baseUrl/$id");
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    final url = Uri.parse("$baseUrl/$id/episodes");
     final response = await http.get(url);
 
+    List<WebtoonEpisodeModel> episodeInstances = [];
+
     if (response.statusCode == 200) {
-      final webtoon = jsonDecode(response.body);
-      return WebtoonDetailModel.fromJson(webtoon);
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        var episodeModel = WebtoonEpisodeModel.fromJson(episode);
+        episodeInstances.add(episodeModel);
+      }
+      return episodeInstances;
     }
     throw Error();
   }
