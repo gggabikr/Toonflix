485cab2743affd22c65032bc768f60256fcf4637
Jason Sejik Lee <gggabikr@gmail.com>
2024-07-25 21:34:11 -0700

    Update episode_widget.dart

--- toonflix/lib/widgets/episode_widget.dart
+++ toonflix/lib/widgets/episode_widget.dart
@@ -1,52 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class Episode extends StatelessWidget {
-  const Episode({
-    super.key,
-    required this.episode,
-  });
+  const Episode({super.key, required this.episode, required this.webtoonId});
 
+  final String webtoonId;
   final WebtoonEpisodeModel episode;
 
   onButtonTap() async {
-    final url = Uri.parse("https://google.ca");
-    await launchUrl(url);
+    await launchUrlString(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}");
     //혹은 아래처럼 해도 된다.
-    // await launchUrlString('https://google.ca');
+    // final url = Uri.parse("https://google.ca");
+    // await launchUrl(url);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 2),
-      decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(15)),
-          border: Border.all(color: const Color.fromRGBO(144, 247, 159, 0.7)),
-          color: Colors.green.shade500,
-          boxShadow: const [
-            BoxShadow(
-              blurRadius: 4,
-              offset: Offset(2, 2),
-              color: Color.fromRGBO(0, 0, 0, 0.2),
-            )
-          ]),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 7),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          SizedBox(
-            width: 200,
-            child: Text(
-              episode.title,
-              style: const TextStyle(fontSize: 16),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 2),
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(15)),
+            border: Border.all(color: const Color.fromRGBO(144, 247, 159, 0.7)),
+            color: Colors.green.shade500,
+            boxShadow: const [
+              BoxShadow(
+                blurRadius: 4,
+                offset: Offset(2, 2),
+                color: Color.fromRGBO(0, 0, 0, 0.2),
+              )
+            ]),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 7),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            SizedBox(
+              width: 200,
+              child: Text(
+                episode.title,
+                style: const TextStyle(fontSize: 16),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
-          ),
-          const Icon(Icons.arrow_right_outlined)
-        ]),
+            const Icon(Icons.arrow_right_outlined)
+          ]),
+        ),
       ),
     );
   }
