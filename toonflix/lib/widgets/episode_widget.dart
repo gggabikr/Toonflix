18cf31d1c954e2c157d829aff05c84c6f7a18e55
Jason Sejik Lee <gggabikr@gmail.com>
2024-07-27 16:57:40 -0700

    Update episode_widget.dart

--- toonflix/lib/widgets/episode_widget.dart
+++ toonflix/lib/widgets/episode_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_episode_model.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class Episode extends StatelessWidget {
